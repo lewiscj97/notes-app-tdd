9bfc4645d160989829a2416989c5cde0b29b4fdc
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-22 12:37:54 +0100

    test(notebook): test that notebook can show a specific note

--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -6,7 +6,7 @@ describe Notebook do
 
     it "creates a new note with the title and body which is visible" do
       subject.new_note("This is the title", "and this is the body")
-      expect{subject.show_notes}.to output("Title: This is the title, Body: and this is the body\n").to_stdout
+      expect{ subject.show_notes }.to output("Title: This is the title, Body: and this is the body\n").to_stdout
     end
   end
 
@@ -17,11 +17,18 @@ describe Notebook do
       subject.new_note("This is the title", "and this is the body")
       subject.new_note("This is another title", "and this is the new body")
 
-      expect{subject.all_titles}.to output("Title: This is the title\nTitle: This is another title\n").to_stdout
+      expect{ subject.all_titles }.to output("Title: This is the title\nTitle: This is another title\n").to_stdout
     end
   end
 
   describe "#show_note" do
     it { is_expected.to respond_to(:show_note) }
+
+    it "shows the title and body of a specific note" do
+      subject.new_note("This is the title", "and this is the body")
+      subject.new_note("This is another title", "and this is the new body")
+
+      expect{ subject.show_note("This is the title") }.to output("Title: This is the title\nBody: and this is the body\n").to_stdout
+    end
   end
 end
